32cd9277f851b07898dd522cb27ed5837bd2894f
Katharina <katharina.luger@sitecore.com>
2023-07-09 20:58:04 +0200

    remove blog-template dir

--- blog-template/src/consts.ts
+++
@@ -1,3 +0,0 @@
-export const SITE_NAME = "NimbusNarratives";
-export const SITE_TITLE = "NimbusNarratives | A personal blog by Abdullah Elsayed (@algorizr)";
-export const SITE_DESCRIPTION = "NimbusNarratives | A personal blog by Abdullah Elsayed (@algorizr)";
